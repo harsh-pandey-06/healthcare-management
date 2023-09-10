28e2de446924b390ed6083592c96f014a27be51d
Dhiraj-Agarwal-IITG <dhirajagarwal2018@gmail.com>
2023-09-10 15:00:46 +0530

    Patient Records and Doctor Appointments table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import NewPatient from './components/NewPatient';
+import PatientRecords from './components/Patients/Records';
+import DoctorAppoints from './components/Doctors/Appointments';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <PatientRecords/>
+      <DoctorAppoints/>
     </div>
   );
 }
