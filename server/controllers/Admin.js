f197958afd742135b6edcc19a41a5d7573d7b6c8
ankitup05 <ankitup05@gmail.com>
2023-09-29 00:09:36 +0530

    uddate admin

--- server/controllers/Admin.js
+++ server/controllers/Admin.js
@@ -120,3 +120,42 @@ exports.login = async (req, res) => {
     })
   }
 }
+
+exports.updateAdmin = async (req, res) => {
+  try {
+    const {
+      firstName = "",
+      lastName = "",
+      email = "",
+      mobile = "",
+      id
+    } = req.body
+
+    // Find the profile by id
+    const AdminDetails = await Admin.findById(id)
+
+    const admin = await Admin.findByIdAndUpdate(id, {
+      firstName,
+      lastName,
+      email,
+      mobile
+    })
+    await admin.save()
+
+
+
+
+
+    return res.json({
+      success: true,
+      message: "Profile updated successfully",
+      updatedAdminDetails,
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    })
+  }
+}
