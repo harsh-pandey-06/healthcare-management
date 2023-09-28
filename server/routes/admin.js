d08d5be79a7677e3d37fbec0b4621b7d0e208a0b
ankitup05 <ankitup05@gmail.com>
2023-09-28 11:51:57 +0530

    update route added

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { login, signup } = require("../controllers/Admin");
+const { login, signup, updateAdmin } = require("../controllers/Admin");
 
 router.post("/login", login);
 router.post("/signup", signup);
+router.post("/updateAdmin", updateAdmin);
 
 module.exports = router;
