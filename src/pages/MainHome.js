fac15530dea1161d40c258140d0a7e7cc3f6264a
ankitup05 <ankitup05@gmail.com>
2023-10-04 18:51:24 +0530

    modified home page

--- src/pages/MainHome.js
+++ src/pages/MainHome.js
@@ -1,13 +1,53 @@
 import React from 'react'
+import { useState} from 'react'
 import bgimage from "../assets/iitgbg.jpeg"
-const mainHome = () => {
+import iitglogo from "../assets/iitg_logo.jpg"
+import { Link } from 'react-router-dom'
+
+
+const MainHome = () => {
+
+    const [role,setRole]=useState("patient");
+    const submitHandler=()=>{
+    console.log(role);
+
+    }
+    // console.log(role);
   return (
-    <div className='flex items-center justify-center h-screen bg-cover opacity-50 bg-center' style={{backgroundImage:`url(${bgimage})` }}>
-      <div className='bg-white h-[30rem] w-[30rem] shadow-xl'>
-        
+    <div className='flex items-center justify-center h-screen bg-cover bg-opacity-50 bg-center' style={{backgroundImage:`url(${bgimage})` }}>
+      <div className='bg-white w-[32rem] shadow-xl'>
+        <div className='flex justify-center my-6'>
+            <img src={iitglogo}/> 
+        </div>
+        <div className='flex flex-col p-10'>
+            <div className='font-bold text-3xl'>
+                Login As
+            </div>
+
+            <div className='flex flex-col gap-4 mt-10 ml-6 font-bold'>
+                <label className='flex gap-4'>
+                    <input type="radio" name='role' value="patient" checked={role==="patient"} onClick={()=>setRole("patient")}/>
+                    Patient
+                </label>
+                <label className='flex gap-4'>
+                    <input type="radio" name='role' value="doctor" checked={role==="doctor"} onClick={()=>setRole("doctor")} />
+                    Doctor
+                </label>
+                <label className='flex gap-4'>
+                    <input type="radio" name='role' value="admin" checked={role==="admin"} onClick={()=>setRole("admin")} />
+                    Admin
+                </label>
+            </div>
+            
+            <div className='flex justify-end mt-10'>
+            <Link to={role==="patient"?"patient/login":role==="doctor"?"doctor/login":"admin/login"} type="button" className='bg-blue-500 flex items-center justify-between gap-2 cursor-pointer text-white px-5 py-3 rounded-lg text-sm font-medium mt-4' onClick={submitHandler}>
+                Continue
+              </Link>
+            </div>
+        </div>
       </div>
     </div>
   )
 }
 
-export default mainHome
+export default MainHome
