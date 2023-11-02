ffa91d10fb2579c0f265738ca02e87ca8bdacda6
ankitup05 <ankitup05@gmail.com>
2023-11-02 19:31:36 +0530

    added date,tokenno in appointment

--- src/components/Appointment/ListItem.js
+++ src/components/Appointment/ListItem.js
@@ -1,8 +1,14 @@
+import dayjs from "dayjs";
 import React from "react";
 
 const ListItem = (props) => {
   const { value } = props;
   console.log(value)
+  let slotPairs={ slot8to10:"8 A.M. to 10 A.M.",
+                  slot10to12:"10 A.M. to 12 P.M.",
+                  slot12to2:"12 P.M. to 2 P.M.",
+                  slot2to4:"2 P.M. to 4 P.M.",
+                  slot4to6:"4 P.M. to 6 P.M."};
   return (
     <div className="w-full shadow-xl p-5 rounded-lg">
       <div>
@@ -12,7 +18,10 @@ const ListItem = (props) => {
         <span className="text-lg font-medium">Patient Id: </span>{value.patient.rollno}
       </div>
       <div>
-        <span className="text-lg font-medium">Preferred slot: </span>{value.slot}
+        <span className="text-lg font-medium">Preferred slot: </span>{slotPairs[value.slot]}
+      </div>
+      <div>
+        <span className="text-lg font-medium">Date of Appointment: </span>{dayjs(value.dateOfAppointment).format('D MMM YYYY')}
       </div>
       <div>
         <span className="text-lg font-medium">Department: </span>{value.department ?? "Not specified"}
@@ -20,6 +29,10 @@ const ListItem = (props) => {
       <div>
         <span className="text-lg font-medium">Reason: </span>
         {value.symptoms}
+      </div>
+      <div>
+        <span className="text-lg font-medium">Token: </span>
+        {value.token_no}
       </div>
     </div>
   );
